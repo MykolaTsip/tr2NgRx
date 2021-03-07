75a9ea40a7fc205593d5420c83f6a8f2bd95f0e1
AndriiHaidar <a.haidar@quantumobile.com>
2021-03-07 16:40:16 +0000

    NGRXpart2 add base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tr2NgRx';
+  counter = 0;
+  updateAt: number;
+
+  get disabletButton(): boolean {
+    return this.counter <= 0;
+  }
+
+
+  decrease(): void {
+    this.counter++;
+    this.updateAt = Date.now();
+  }
+
+  increase(): void {
+    this.counter--;
+    this.updateAt = Date.now();
+  }
+
+  clear(): void {
+    this.counter = 0;
+    this.updateAt = Date.now();
+  }
+
+  // ngAfterViewInit(): void {
+  //   this.updateAt = Date.now();
+  // }
 }
